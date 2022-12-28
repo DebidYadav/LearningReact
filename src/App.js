98a0c6fd0d4d63c90abc9c979ac57274abf20aab
Debid <david23n23@gmail.com>
2022-12-28 20:47:03 +0530

    coding food app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1",{},"Hello World");
-console.log(heading);
+title = "Food Piper";
+
+const HeadingComponent = () =>(
+    <div id="title" className="title_class" tabIndex="1">
+        <h2>{title}</h2>
+    </div>
+);
+
+const CardComponent = () =>(
+    <div id="card" className="card">
+        <img src ="https://thumbs.dreamstime.com/z/pizza-slice-5957584.jpg"></img>
+        <h2>Pizza 47</h2>
+        <h3>Pizza, Italian</h3>
+        <h4>4.2 Stars</h4>
+    </div>
+);
+
+const BodyComponent = () =>(
+    <div className="card-container">
+        <CardComponent/>
+        <CardComponent/>
+        <CardComponent/>
+        <CardComponent/>
+        <CardComponent/>
+    </div>
+);
+
+const AppLayout = () =>(
+    <>
+        <HeadingComponent/>
+        <BodyComponent/>
+    </>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<AppLayout/>);
